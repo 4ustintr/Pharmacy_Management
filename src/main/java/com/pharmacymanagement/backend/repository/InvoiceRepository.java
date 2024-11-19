95f799b60655a089c568ef0e7e8cbf80b453b879
4ustintr <petterpacker2@gmail.com>
2024-11-19 08:42:12 +0700

    update repo

--- src/main/java/com/pharmacymanagement/backend/repository/InvoiceRepository.java
+++ src/main/java/com/pharmacymanagement/backend/repository/InvoiceRepository.java
@@ -2,6 +2,8 @@ package com.pharmacymanagement.backend.repository;
 
 import com.pharmacymanagement.backend.model.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 }
--- src/main/java/com/pharmacymanagement/backend/repository/PatientRepository.java
+++ src/main/java/com/pharmacymanagement/backend/repository/PatientRepository.java
@@ -2,6 +2,8 @@ package com.pharmacymanagement.backend.repository;
 
 import com.pharmacymanagement.backend.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 }
